fe3afbed8eadbb1405a487aee24e4aa7090c1cf7
mbragin <bangvalo@gmail.com>
2014-04-21 09:24:46 -0300

    added UI Main project

---
+++ UI Main/Assets/UI Main/Scripts/ResolutionController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Script used to load proper atlas based on device resolution
+ * 
+**/
+public class ResolutionController : MonoBehaviour {
+
+	public GameObject replacementAtlas;
+	public string fullAtlasName = "Atlas - UI Background Full";
+	public string smallAtlasName = "Atlas - UI Background Small";
+	public UIAtlas referenceAtlas;
+	public bool loadFull;//for test
+
+
+	// Use this for initialization
+	void Start () {
+		if (!referenceAtlas)
+		{
+			Debug.LogError("Reference Atlas was not found");
+			Application.Quit();
+		}
+
+
+		string chosenAtlas = loadFull ? fullAtlasName : smallAtlasName;
+		Debug.Log("Chose Atlas " + chosenAtlas);
+
+		replacementAtlas = Resources.Load(chosenAtlas, typeof (GameObject)) as GameObject;
+
+		if (replacementAtlas) 
+		{
+			referenceAtlas.replacement = replacementAtlas.GetComponent<UIAtlas>();
+		}
+		else 
+		{
+			Debug.LogError("No Replacement Atlas was found");
+		}
+	
+	}
+}
