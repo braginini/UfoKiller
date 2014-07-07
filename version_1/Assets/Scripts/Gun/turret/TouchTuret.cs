75d9227ce04fa09955877358a331099989c2d026
mikhail.bragin <mikhail@grupoxago.com>
2014-07-07 12:52:30 -0300

    part 12

--- version_1/Assets/Scripts/Gun/turret/TouchTuret.cs
+++ version_1/Assets/Scripts/Gun/turret/TouchTuret.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class TouchTuret : Turret
 {
+	public float angleThreshold = .3f;
+	public string fireButtonName = "Turret Button";
+	private Vector2 prevJoysticValue = new Vector2(0f, 0f);
+	public float clampXMin = -60f;
+	public float clampXMax = 30f;
+	public float clampYMin = -70f;
+	public float clampYMax = 70f;
 
     void OnEnable()
     {
         EasyButton.On_ButtonPress += On_ButtonPress;
         EasyButton.On_ButtonUp += On_ButtonUp;
+		EasyJoystick.On_JoystickMove += On_JoystickMove;
     }
 
 
@@ -15,11 +23,19 @@ public class TouchTuret : Turret
     {
         EasyButton.On_ButtonPress -= On_ButtonPress;
         EasyButton.On_ButtonUp -= On_ButtonUp;
+		EasyJoystick.On_JoystickMove -= On_JoystickMove;
     }
+
+	void OnDestroy()
+	{
+		EasyJoystick.On_JoystickMoveEnd -= On_JoystickMove;
+		EasyButton.On_ButtonPress -= On_ButtonPress;
+		EasyButton.On_ButtonUp -= On_ButtonUp;
+	}
 
     void On_ButtonPress(string buttonName)
     {
-        if (buttonName == "TurretButton")
+		if (buttonName == fireButtonName)
         {
             base.Fire();    
         }
@@ -32,4 +48,22 @@ public class TouchTuret : Turret
             
         }
     }
+
+	/**
+	 * handles EasyTouch joystic move event
+	**/
+	void On_JoystickMove(MovingJoystick move)
+	{
+		float angle = Mathf.Abs(Vector2.Angle(move.joystickValue, prevJoysticValue));
+		if (angle < angleThreshold) {
+			return;
+		}
+		
+		Debug.Log("MOVE" + move.joystickValue + " angle=" + angle);
+		prevJoysticValue = move.joystickValue;
+
+		transform.eulerAngles = new Vector3(Mathf.Clamp(move.joystickValue.y, clampXMin, clampXMax), 
+		                                    Mathf.Clamp(move.joystickValue.x, clampYMin, clampYMax), 
+		                                    0.0f);
+	}
 }
