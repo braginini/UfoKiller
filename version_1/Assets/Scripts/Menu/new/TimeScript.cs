cff62407fd6e800ba6c812dedf5399abb43b68c7
Andrey <andpar83@gmail.com>
2014-07-07 16:42:48 -0300

    Added timer

---
+++ version_1/Assets/Scripts/Menu/new/TimeScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeScript : MonoBehaviour
+{
+	private UILabel minutesLabel = null;
+	private UILabel secondsLabel = null;
+	
+	void Start()
+	{
+		minutesLabel = GameObject.Find("Minutes Label").GetComponent<UILabel>();
+		secondsLabel = GameObject.Find("Seconds Label").GetComponent<UILabel>();
+	}
+	
+	void Update()
+	{
+		var t = (int)Time.timeSinceLevelLoad;
+		var seconds = t % 60;
+		var minutes = (t / 60) % 100; // Just 2 digits for minutes
+		minutesLabel.text = minutes.ToString("00");
+		secondsLabel.text = seconds.ToString("00");
+	}
+}
